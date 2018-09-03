6c39d8b15dd4d45f420c18fff0bffa5288a44c04
David Mendelovits <david.mendelovits@gmail.com>
2018-09-02 19:07:28 -0700

    hurr

--- ft_itoa.c/ft_itoa.c
+++ ft_itoa.c/ft_itoa.c
@@ -1,51 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct		s_number
+{
+	int		length;
+	int		is_negative:1;
+	int		number;
+}			t_number;
+
 static int	number_len(int number)
 {
-	printf("function -> number_len: %d\n", number);
-
-	int	div;
-	int	len;
-
-	div = 10;
-	len = 0;
-	while (number / div > 0)
+	int		length;
+	
+	length = 1;
+	while (number >= 10)
 	{
-		printf("%d /= %d ->", number, div);
-		number /= div;
-		div *= 10;
-		printf("%d", number);
-		len += 1;
-		if (!number)
-		{
-			break ;
-		}
+		number /= 10;
+		length += 1;
 	}
-	return (len);
+	return (length);
 }
 
 char		*ft_itoa(int number)
 {
 	int		div;
 	char		*string;
-	int		string_ptr;
 	int		number_length;
-
-	div = 10;
-	number_length = 
-	while (number > 0)
+	int		string_ptr;
+	
+	number_length = 0;
+	if (number < 0)
 	{
-		break ;	
+		number *= -1;
+		number_length = 1;
 	}
-	return ("placeholder\n");
+	number_length += number_len(number);
+	printf("number: %d\n", number_length);
+	return ("placeholder");
 }
 
 int		main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		printf("int to string: %s\n", ft_itoa(atoi(argv[1])));	
+		printf("\nint to string: %s\n", ft_itoa(atoi(argv[1])));	
 	}
 	return (0);
 }
